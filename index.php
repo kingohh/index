b03ff2fc023e4a3285e63db075cfc9643faf1c5b
hamza <boukioudhamza05@gmail.com>
2024-10-16 22:07:14 +0100

    Ajout du style css directement dans index.php

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        h1{
+            color:black;
+        }
+        body{
+            background-color: #f2f2f2;
+        }
+        p{
+            color: #000000;
+            font: size 1.2em;
+        }
+    </style>
 </head>
 <body>
     <h1>Bienvenu dans notre site web</h1>
