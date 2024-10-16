72588990f5de17b95ed6655729ad6838f9f1a2e4
hamza <boukioudhamza05@gmail.com>
2024-10-16 18:27:02 +0100

    Ajout du fichier index.php en implementant la page d'acceuil

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenu dans notre site web</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet nihil molestiae quia itaque, laboriosam in minus sequi dicta. Adipisci voluptatum ea sequi deserunt in minima officia quisquam harum ullam culpa.</p>
+</body>
+</html>
